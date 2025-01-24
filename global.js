5591e804f30149560b7cd893516463e5283e2e29
Kevin Wu <kew036@ucsd.edu>
2025-01-23 18:57:08 -0800

    style updated

--- global.js
+++ global.js
@@ -3,3 +3,48 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+// navLinks = $$("nav a");
+
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname
+//   );
+
+//   if (currentLink) {
+//     // or if (currentLink !== undefined)
+//     currentLink?.classList.add('current');
+//   }
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'resume/', title: 'Resume' },
+    { url: 'contact/', title: 'Contact' }
+    // add the rest of your pages here
+  ];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    // TODO create link and add it to nav
+    // Create link and add it to nav
+    if (!ARE_WE_HOME && !url.startsWith('http')) {
+        url = '../' + url;
+    }
+
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
+    
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+    }
+  }
+
+  
